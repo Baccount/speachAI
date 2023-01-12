ad1d389646a909a44223d5ec5071d27b7e7dca36
“Brandon <cassias.long0r@icloud.com>
2023-01-12 14:29:14 -0800

    first working build

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 # convert users voice to text
 import speech_recognition as sr
+from getResponse import askAI
+import subprocess
 
 # create recognizer and mic instances
 r = sr.Recognizer()
 mic = sr.Microphone()
+while True:
+    # get microphone input
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        print("Say something!")
+        audio = r.listen(source)
 
-# get microphone input
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    print("Say something!")
-    audio = r.listen(source)
-
-# transcribe speech to text
-text = r.recognize_google(audio)
-# print the transcribed text
-print(f"You said: {text}")
-
-
-# upload the text to open ai and get the response
+    # transcribe speech to text
+    text = r.recognize_google(audio)
+    # print the transcribed text
+    print(f"You said: {text}")
 
 
-# speak the response
+    # upload the text to open ai and get the response
+    response = askAI(question=text)
+
+    # speak the response
+    print(response)
+    
+    subprocess.run(["say", response])
